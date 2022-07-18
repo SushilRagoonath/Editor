80c4e426b3008dd16f52b193eaa02c5d4c3372b4
SushilRagoonath <48589447+SushilRagoonath@users.noreply.github.com>
2022-07-18 19:52:54 +0200

    clean up

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,4 @@
 #include <blah.h>
-#include <imgui.h>
 using namespace Blah;
 struct Editor{
     int width =500;
@@ -11,41 +10,24 @@ struct Editor{
     TextureRef main_tex;
     Vector<Subtexture> subs;
     Vec2f tile_size = Vec2f(8.f,8.f);
+    bool tile_picking = false;
     int * data = nullptr;
     void init();
     void update();
     void save();
     void tile_picker();
+    void draw_map();
     bool in_range(Vec2f test);
     Batch b;
     bool changed = false;
     int current = -1;
 };
-struct UI{
-    struct Element{
-        // meta
-        String name;
-        Rectf rect;
-        Color bg;
-        Color fg;
-        Subtexture tex;
-        // data to be changed for user
-        bool pressed;
-        bool toggle;
-        float valf;
-        int vali;
-        String vals;
-    };
-    Vector<Element> elements;
-    void add(Vec2f pos,String name);
-    void render();
-    Batch batch;
-};
 Batch batch;
 SpriteFont font;
 void Editor::init(){
-    main_tex = Texture::create("platformer.png");
     font = SpriteFont("default.ttf",15);
+    // TODO image specific
+    main_tex = Texture::create("platformer.png");
     for (int i = 0; i < 8; ++i) {
         for (int j = 0; j < 8; ++j) {
             subs.push_back(Subtexture(main_tex,Rectf(tile_size.x*j,tile_size.y*i,tile_size.x,tile_size.y)));
@@ -64,11 +46,16 @@ void Editor::update(){
     App::backbuffer()->clear(Color::black);
     changed = false;
     int m_x,m_y;
+    // aligns mouse pick to grid
     m_x = int(Calc::floor(mouse_pos.x/tile_size.x));
     m_y = int(Calc::floor(mouse_pos.y/tile_size.y));
-
-
-    if(Input::down(MouseButton::Left) && in_range(mouse_pos)){
+    if(Input::pressed(Key::Space)){
+        tile_picking = !tile_picking;
+    }
+    if(tile_picking){
+        tile_picker();
+    }
+    else if(Input::down(MouseButton::Left) && in_range(mouse_pos)){
         changed = true;
         data[m_y*width + m_x] = current;
     }
@@ -80,25 +67,9 @@ void Editor::update(){
         offset += Input::mouse() - mouse_prev;
     }
 
-    zoom -= float(Input::mouse_wheel().y)  *0.05;
-    zoom = Calc::clamp(zoom,0.001,2000);
-    Vec2f center = Vec2f(0,0);
-    auto transform = Mat3x2f::create_transform(center+offset, Vec2f::zero, Vec2f(1/zoom,1/zoom), 0);
-    batch.push_matrix(transform);
-    mouse_pos = Vec2f::transform(Input::mouse(),batch.peek_matrix().invert());
-    batch.set_sampler( TextureSampler(TextureFilter::Nearest));
-    for (int i = 0; i < height; ++i) {
-        for (int j = 0; j < width; ++j) {
-            int index = j*width + i;
-            Vec2f pos = Vec2f(8*i,8*j);
-            int id = data[index];
-//            batch.rect_line(Rectf(8*i,8*j,8,8),0.5,Color::white);
-            if(id!=-1)
-                batch.tex(subs[id],pos,Vec2f::zero,Vec2f(1.f,1.f),0,Color::white);
-        }
-    }
-    batch.pop_matrix();
-    tile_picker();
+    zoom -= float(Input::mouse_wheel().y) * 0.05;
+    zoom = Calc::clamp(zoom,0.001,2000); // dont want the image to be reversed with -zoom
+    if(!tile_picking) draw_map();
     batch.str(font,String::fmt("%2d %2d\n",m_x,m_y),Vec2f(10,10),Color::white);
 
     batch.render();
@@ -109,7 +80,7 @@ void Editor::update(){
 void Editor::save() {
 //    tex.
 }
-
+// check pos is in range of image
 bool Editor::in_range(Vec2f test) {
     int x,y;
     x = int(Calc::floor(test.x));
@@ -119,18 +90,48 @@ bool Editor::in_range(Vec2f test) {
 }
 
 void Editor::tile_picker() {
+    batch.set_sampler( TextureSampler(TextureFilter::Nearest));
 
-    batch.push_matrix(Mat3x2f::create_transform(Vec2f(300,0),Vec2f::zero,Vec2f(10,10),0));
+    batch.push_matrix(Mat3x2f::create_transform(Vec2f(main_tex->width()/2,main_tex->height()/2),Vec2f::zero,Vec2f(10,10),0));
+    // mouse picking
     Vec2 mouse_picker = Vec2f::transform(Input::mouse(),batch.peek_matrix().invert());
     int m_x,m_y;
     m_x = int(Calc::floor(mouse_picker.x));
     m_y = int(Calc::floor(mouse_picker.y));
-    batch.circle(Vec2f(m_x,m_y),10,20,Color::white);
     for (int i = 0; i < subs.size(); ++i) {
-//        batch.rect(subs[i].source,Color::white);
-//        batch.tex(subs[i],);
+        batch.tex(subs[i],subs[i].source.top_left());
+        // hover
+        if(subs[i].source.contains(Vec2f(m_x,m_y))){
+            batch.rect(subs[i].source,Color(134,103,255,64));
+        }
+        // button test
+        if(subs[i].source.contains(Vec2f(m_x,m_y)) && Input::pressed(MouseButton::Left)){
+            current = i;
+        }
+    }
+    // outline for selected
+    batch.rect_line(subs[current].source,0.5,Color::orange);
+    batch.pop_matrix();
+}
+
+void Editor::draw_map() {
+    Vec2f center = Vec2f(0,0);
+    auto transform = Mat3x2f::create_transform(center+offset, Vec2f::zero, Vec2f(1/zoom,1/zoom), 0);
+    batch.push_matrix(transform);
+    // mouse pick
+    mouse_pos = Vec2f::transform(Input::mouse(),batch.peek_matrix().invert());
+    batch.set_sampler( TextureSampler(TextureFilter::Nearest));
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; ++j) {
+            int index = j*width + i;
+            Vec2f pos = Vec2f(8*i,8*j);
+            int id = data[index];
+            if(id!=-1) // -1 invalid
+                batch.tex(subs[id],pos,Vec2f::zero,Vec2f(1.f,1.f),0,Color::white);
+        }
     }
     batch.pop_matrix();
+
 }
 
 
